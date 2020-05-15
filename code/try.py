903d9f5f812b7a0e2d9c5d4b350ec60babef4ed4
Your Name <you@example.com>
2020-05-14 20:22:24 -0500

    multi_threading

--- code/try.py
+++ code/try.py
@@ -1,10 +1,12 @@
 import requests
-import matplotlib.pyplot as plt
-import json
 import numpy as np
 import shutil
 import os
 from tqdm import tqdm
+import threading
+import time
+
+start = time.perf_counter()
 
 
 def download_aerial_img(latitude, longitude, filename):
@@ -45,15 +47,15 @@ def get_aerial_img(bounding_box):
     if 'images' in os.listdir('../'):
         shutil.rmtree('../images')
     min_lat, max_lat, min_lon, max_lon = bounding_box[0], bounding_box[1], bounding_box[2], bounding_box[3]
-    sample_interval = 0.00035
+    sample_interval = 0.0004
 
-    num_lat = int((max_lat-min_lat)/sample_interval)
-    num_long = int((max_lon-min_lon)/sample_interval)
+    num_lat = int((max_lat - min_lat) / sample_interval)
+    num_long = int((max_lon - min_lon) / sample_interval)
 
     latitudes = np.linspace(start=min_lat, stop=max_lat, num=num_lat)
     longitudes = np.linspace(start=min_lon, stop=max_lon, num=num_long)
 
-    interval = latitudes[1]-latitudes[0]
+    # interval = latitudes[1] - latitudes[0]
     # print(interval)
 
     # print(f'latitudes is {latitudes}')
@@ -61,13 +63,26 @@ def get_aerial_img(bounding_box):
 
     os.mkdir('../images')
 
-    for idx_lat, latitude in tqdm(enumerate(latitudes)):
-        for idx_long, longitude in tqdm(enumerate(longitudes)):
+    threads = []
+    for idx_lat, latitude in enumerate(latitudes):
+        # time.sleep(2)
+        for idx_long, longitude in enumerate(longitudes):
             filename = f'img_{idx_lat}_{idx_long}'
-            download_aerial_img(latitude, longitude, filename)
+
+            t = threading.Thread(target=download_aerial_img, args=[latitude, longitude, filename])
+            t.start()
+            threads.append(t)
+            # download_aerial_img(latitude, longitude, filename)
+
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == '__main__':
     boundary = [42.048957, 42.063487, -87.680418, -87.669603, ]
 
     get_aerial_img(boundary)
+
+    finish = time.perf_counter()
+
+    print(f'Finished in {round(finish - start, 2)} second(s)')
