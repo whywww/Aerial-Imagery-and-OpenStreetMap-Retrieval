62e1d79aa918118f36dac71731bc3b9e5ba6f8c7
Your Name <you@example.com>
2020-05-19 15:24:22 -0500

    getmap

--- code/get_aerial.py
+++ code/get_aerial.py
@@ -23,7 +23,7 @@ def get_my_map(mapArea, mapSize):
 
     os.mkdir('../images')
 
-    with open(f'../images/my_map.jpg', 'wb') as file:
+    with open(f'../images/my_map_unprocessed.jpg', 'wb') as file:
         file.write(r.content)
 
 
@@ -45,21 +45,61 @@ def get_metadata(mapArea, mapSize):
     return bounding_box, map_center
 
 
-def process_my_map():
-    pass
+def process_my_map(bounding_box, map_area):
+    my_map = cv2.imread('../images/my_map_unprocessed.jpg')
+    if my_map is None:
+        print('No map loaded!')
+        return
+
+    height, width = my_map.shape[0], my_map.shape[1]
+    print(bounding_box)
+    print(map_area)
+
+    lat_step = (bounding_box[2] - bounding_box[0]) / height
+    long_step = (bounding_box[3] - bounding_box[1]) / width
+    top, left, bottom, right = 0, 0, 0, 0
+
+    print(lat_step)
+    print(long_step)
+
+    for i in range(height):
+        current_lat = bounding_box[0] + i*lat_step  # adding from the south to north
+        if current_lat > map_area[2]:
+            bottom = i
+            break
+
+    for i in range(height):
+        current_lat = bounding_box[0] + i*lat_step  # adding from the south to north
+        if current_lat > map_area[0]:
+            top = i
+            break
+
+    for i in range(width):
+        current_long = bounding_box[1] + i*long_step  # adding from the east to west
+        if current_long > map_area[3]:
+            right = i
+            break
+
+    for i in range(width):
+        current_long = bounding_box[1] + i*long_step  # adding from the east to west
+        if current_long > map_area[1]:
+            left = i
+            break
+
+    my_map = my_map[top:bottom, left:right]
+
+    cv2.imwrite('../images/my_map.png', my_map)
 
 
 if __name__ == '__main__':
     # the area must have the format of [South Latitude, West Longitude, North Latitude, East Longitude]
-    map_area = [42.053140, -87.676687, 42.060023, -87.672052]
+    map_area = [42.050993, -87.679421, 42.061049, -87.670194]
 
     # width ranges between 80 and 2000, height ranges between 80 and 1500 (pixels)
     # if not set, default will be 350*350 pixel
     map_size = [2000, 1500]
 
-    # get_my_map(map_area, map_size)
+    get_my_map(map_area, map_size)
     bounding_box, center = get_metadata(map_area, map_size)
-
     # bounding box has the format of [South Latitude, West Longitude, North Latitude, East Longitude]
-
-
+    process_my_map(bounding_box, map_area)
